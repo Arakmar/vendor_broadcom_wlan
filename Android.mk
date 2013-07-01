76601512b34b30445593178ca887ffb08d11a390
RaymanFX <raymanfx@gmail.com>
2013-07-01 16:24:06 +0200

    modify build conditions

--- Android.mk
+++ Android.mk
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-ifdef BOARD_WLAN_BROADCOM
+ifeq ($(BOARD_WLAN_BROADCOM),true)
 
 LOCAL_PATH:= $(call my-dir)
 
